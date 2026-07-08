aca043cebcd5329a98673c280c564c944b170ae4
Didi692 <dianachavezsmr@gmail.com>
2026-07-08 18:44:42 -0500

    Add options for user selection and responses

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-# Mis-practicas
+st.set_page_config(page_title="Mi Rincon de Practicas", page_icon="💗", layout="centered")
+st.title("🌺 Bienvenido a mi espacio de pruebas")
+st.write("Aqui voy a practicar programacion y crear cosas que me gustan")
+st.subheader("¿Que serie o grupo descubriremos hoy?")
+opcion = st.selectbox(
+  "Elige una opcion para ver un mensaje especial:",
+  ["Selecciona uno...", "K-dramas Romanticos", "Tiktok", "Flores favoritas"]
+)
+if option == "K-dramas Romanticos":
+  st.success("¡Una excelente eleccion! Nada como una buena historia de amor.")
+elif option == "Tiktok":
+  st.info("Si te distrae perfecto, pero no mucho tiempo plis Diana, sabe que tiene que estudiar")
+elif option == "Flores favoritas":
+  st.write("Las flores como las peonías son las mejores ¡No te rindas!")
+  
+st.subheader("El rincon de las personalidades")
+st.write("Te apasiona el MBTI o el Eneagrama? ¡Pronto hare un test interactivo aqui!")
